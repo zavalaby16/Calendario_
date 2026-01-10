241e42b679ece8b65063f13367e8f4a9548dfa37
zavalaby16 <zavalaby@gmail.com>
2026-01-09 23:14:05 -0600

    Refactor service worker caching logic

--- sw.js
+++ sw.js
@@ -1,10 +1,24 @@
-self.addEventListener('install', event => {
+const CACHE_NAME = "calendario-v1";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open('calendario-v1').then(cache => {
-      return cache.addAll([
-        './',
-        './index.html'
-      ]);
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
     })
   );
 });
+
